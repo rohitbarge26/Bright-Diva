1944d62491157e425c336c2469ef97574d90fefc
rbarge-paktolus <rbarge@paktolus.com>
2024-11-27 11:16:45 +0530

    add fonts

---
+++ lib/authentication/screens/login_email_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginEmailScreen extends StatefulWidget {
+  const LoginEmailScreen({super.key});
+
+  @override
+  State<LoginEmailScreen> createState() => _LoginEmailScreenState();
+}
+
+class _LoginEmailScreenState extends State<LoginEmailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
