f46aedc78b3e69b3e550f66e13818e9e5bbed827
rbarge-paktolus <rbarge@paktolus.com>
2024-12-19 14:15:56 +0530

    update

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,5 +1,5 @@
 const String BASE_URL = "http://192.168.1.5:9000/api/";
-// const String BASE_URL = "http://192.168.34.182:9000/api/";
+//const String BASE_URL = "http://192.168.34.182:9000/api/";
 
 const String LOGIN = "v1/login";
 const String CHANGE_PASSWORD = "v1/user/change-password";
